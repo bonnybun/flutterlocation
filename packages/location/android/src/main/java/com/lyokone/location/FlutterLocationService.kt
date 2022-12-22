f366abd86bdefe0d3b8f52203edb6b6bce2fe223
bonnybunadmin <bonny.bun@accopilot.com>
2022-12-22 10:20:38 +0100

    add PendingIntent.FLAG_MUTABLE flag

--- packages/location/android/src/main/java/com/lyokone/location/FlutterLocationService.kt
+++ packages/location/android/src/main/java/com/lyokone/location/FlutterLocationService.kt
@@ -54,7 +54,19 @@ class BackgroundNotification(
                 ?.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
 
         return if (intent != null) {
-            PendingIntent.getActivity(context, 0, intent, 0)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.getActivity(
+                        context,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+                )
+            } else {
+                PendingIntent.getActivity(
+                        context,
+                        0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            }
         } else {
             null
         }
